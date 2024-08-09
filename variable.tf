764726768cb989273a1d0eb67989600a8e34f083
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-08-09 09:25:04 +0530

    vpc

--- variable.tf
+++ variable.tf
@@ -1,3 +1,16 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environemnt" {
+  type = string
+  default = "dev"
+  
+}
+variable "common_tags" {
+  type = map
+  
+}
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
@@ -6,4 +19,9 @@ variable "vpc_cidr" {
 variable "enable_dns_hostnames" {
     type = bool
     default = true
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {}
 }
--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,11 @@ resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = var.enable_dns_hostnames
-  tags = {
-    Name = "main"
-  }
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+      Name = "${var.project_name}-${var.environemnt}"
+    }
+  )
 }
